4d73ed2ce0b2e93027d1a5b00fba836d301a6d4a
R-Studios <Roman.Wecker@gmx.de>
2018-08-30 17:06:47 +0200

    Control Setup #0
    Basic Movement
    Jump Mechanic

---
+++ Assets/Scripts/Ingame/Player/Character.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float speed = 3;
+
+    public InputReader.Controller controller;
+    public Rigidbody2D rigidbody;
+
+    private bool cooldownJumpInput;
+    private float timerJumpInput;
+
+    private bool aRelease;
+
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Horizontal: "+InputReader.getInput(controller, InputReader.ControlType.HORIZONTAL));
+        //Debug.Log("Vertical  : " + InputReader.getInput(controller, InputReader.ControlType.VERTICAL));
+        //Debug.Log("A         : " + InputReader.getInput(controller, InputReader.ControlType.A_BTN));
+        //Debug.Log("B         : " + InputReader.getInput(controller, InputReader.ControlType.B_BTN));
+        //Debug.Log("X         : " + InputReader.getInput(controller, InputReader.ControlType.X_BTN));
+        //Debug.Log("Y         : " + InputReader.getInput(controller, InputReader.ControlType.Y_BTN));
+
+        if (InputReader.getInput(controller, InputReader.ControlType.B_BTN) == 1)
+        {
+            secondary();
+            return;
+        }
+
+        if (InputReader.getInput(controller, InputReader.ControlType.HORIZONTAL) == 1)
+            moveRight();
+        else if (InputReader.getInput(controller, InputReader.ControlType.HORIZONTAL) == -1)
+            moveLeft();
+        else
+            slow();
+
+        handleButtons();
+
+    }
+
+    private void handleButtons()
+    {
+        if (!aRelease && InputReader.getInput(controller, InputReader.ControlType.A_BTN) == 0)
+            aRelease = true;
+
+        if (timerJumpInput == 0)
+        {
+            if (cooldownJumpInput)
+            {
+                if (InputReader.getInput(controller, InputReader.ControlType.A_BTN) == 1)
+                {
+                    //TODO: asadssa
+                    if (InputReader.getInput(controller, InputReader.ControlType.VERTICAL) == 1)
+                    {
+                        save();
+                    }
+                    else
+                    {
+                        jump();
+                    }
+                    timerJumpInput = 0.1f;
+                    cooldownJumpInput = false;
+                    aRelease = false;
+                }
+                else {
+                    cooldownJumpInput = false;
+                }
+            }
+            else if(aRelease && InputReader.getInput(controller, InputReader.ControlType.A_BTN) == 1)
+            {
+                timerJumpInput = 0.03f;
+                cooldownJumpInput = true;
+            }
+        }
+        else
+        {
+            timerJumpInput -= Time.deltaTime;
+            if (timerJumpInput < 0)
+                timerJumpInput = 0;
+        }
+        
+    }
+
+    public virtual void slow()
+    {
+        //Debug.Log("Slow");
+    }
+
+    public virtual void moveRight()
+    {
+        Debug.Log("Right");
+    }
+
+    public virtual void moveLeft()
+    {
+        Debug.Log("Left");
+    }
+
+    public virtual void jump()
+    {
+        Debug.Log("A");
+    }
+
+    public virtual void primary()
+    {
+        Debug.Log("X");
+    }
+
+    public virtual void secondary()
+    {
+        Debug.Log("B");
+    }
+
+    public virtual void save()
+    {
+        Debug.Log("V");
+    }
+}
---
+++ Assets/Scripts/Ingame/Player/InputReader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReader {
+
+    public enum ControlType
+    {
+        HORIZONTAL, VERTICAL, A_BTN, B_BTN, X_BTN, Y_BTN
+    }
+
+    public enum Controller
+    {
+        ONE, TWO, THREE, FOUR
+    }
+
+    public static float getInput(Controller controller, ControlType controltype)
+    {
+        switch(controltype) {
+            case ControlType.HORIZONTAL: {
+                    float input = Input.GetAxis("Horizontal_" + controller.ToString());
+                    if (input > 0.25)
+                        return 1;
+                    else if (input < -0.25)
+                        return -1;
+                    return 0;
+                }
+            case ControlType.VERTICAL:
+                {
+                    float input = Input.GetAxis("Vertical_" + controller.ToString());
+                    if (input > 0.6)
+                        return 1;
+                    else if (input < -0.6)
+                        return -1;
+                    return 0;
+                }
+            case ControlType.A_BTN:
+                {
+                    return Input.GetAxis("A_BTN_" + controller.ToString());
+                }
+            case ControlType.B_BTN:
+                {
+                    return Input.GetAxis("B_BTN_" + controller.ToString());
+                }
+            case ControlType.X_BTN:
+                {
+                    return Input.GetAxis("X_BTN_" + controller.ToString());
+                }
+            case ControlType.Y_BTN:
+                {
+                    return Input.GetAxis("Y_BTN_" + controller.ToString());
+                }
+        }
+        return 0;
+    }
+
+}
