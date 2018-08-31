6e935746941678b238746257840dc84814bcf7a5
R-Studios <Roman.Wecker@gmx.de>
2018-08-31 17:53:22 +0200

    Multiple Controller Support

---
+++ Assets/Scripts/Ingame/Player/ControllerHub.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class ControllerHub : MonoBehaviour {
+
+    public static ControllerHub Instance;
+    public ArrayList a = new ArrayList();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void verifyControllers()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            PlayerIndex testPlayerIndex = (PlayerIndex)i;
+            GamePadState testState = GamePad.GetState(testPlayerIndex);
+            if (testState.IsConnected)
+            {
+                if (!a.Contains(testPlayerIndex))
+                    a.Add(testPlayerIndex);
+            }
+            else if (a.Contains(testPlayerIndex))
+                a.Remove(testPlayerIndex);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        verifyControllers();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        verifyControllers();
+    }
+
+    public PlayerIndex[] GetControls()
+    {
+        PlayerIndex[] array = new PlayerIndex[4];
+        for (int i = 0; i < 4; i++)
+        {
+            if (a.Count > i)
+            {
+                if (a[i] != null)
+                {
+                    array[i] = (PlayerIndex) a[i];
+                }
+            }
+        }
+        return array;
+    }
+}
--- Assets/Scripts/Ingame/Player/InputReader.cs
+++ Assets/Scripts/Ingame/Player/InputReader.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class InputReader {
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
 
     public enum ControlType
     {
@@ -14,12 +20,46 @@ public class InputReader {
         ONE, TWO, THREE, FOUR
     }
 
+    void FixedUpdate()
+    {
+        // SetVibration should be sent in a slower rate.
+        // Set vibration according to triggers
+        GamePad.SetVibration(playerIndex, state.Triggers.Left, state.Triggers.Right);
+    }
+
     public static float getInput(Controller controller, ControlType controltype)
     {
-        switch(controltype) {
-            case ControlType.HORIZONTAL: {
-                    float input = Input.GetAxis("Horizontal_" + controller.ToString());
-                    //Debug.Log(controller.ToString());
+        PlayerIndex testPlayerIndex = (PlayerIndex)0;
+        GamePadState state = GamePad.GetState(testPlayerIndex);
+
+        switch (controller)
+        {
+            case Controller.ONE: {
+                    state = GamePad.GetState(ControllerHub.Instance.GetControls()[0]);
+                    break;
+                }
+            case Controller.TWO:
+                {
+                    state = GamePad.GetState(ControllerHub.Instance.GetControls()[1]);
+                    break;
+                }
+            case Controller.THREE:
+                {
+                    state = GamePad.GetState(ControllerHub.Instance.GetControls()[2]);
+                    break;
+                }
+            case Controller.FOUR:
+                {
+                    state = GamePad.GetState(ControllerHub.Instance.GetControls()[3]);
+                    break;
+                }
+        }
+
+        switch(controltype)
+        {
+            case ControlType.HORIZONTAL:
+                {
+                    float input = state.ThumbSticks.Left.X;
                     if (input > 0.25)
                         return 1;
                     else if (input < -0.25)
@@ -28,32 +68,31 @@ public class InputReader {
                 }
             case ControlType.VERTICAL:
                 {
-                    float input = Input.GetAxis("Vertical_" + controller.ToString());
-                    //Debug.Log(controller.ToString());
-                    if (input > 0.6)
+                    float input = state.ThumbSticks.Left.Y;
+                    if (input > 0.25)
                         return 1;
-                    else if (input < -0.6)
+                    else if (input < -0.25)
                         return -1;
                     return 0;
                 }
             case ControlType.A_BTN:
                 {
-                    return Input.GetAxis("A_BTN_" + controller.ToString());
+                    return (state.Buttons.A == ButtonState.Pressed) ? 1 : 0;
                 }
             case ControlType.B_BTN:
                 {
-                    return Input.GetAxis("B_BTN_" + controller.ToString());
+                    return (state.Buttons.B == ButtonState.Pressed) ? 1 : 0;
                 }
             case ControlType.X_BTN:
                 {
-                    return Input.GetAxis("X_BTN_" + controller.ToString());
+                    return (state.Buttons.X == ButtonState.Pressed) ? 1 : 0;
                 }
             case ControlType.Y_BTN:
                 {
-                    return Input.GetAxis("Y_BTN_" + controller.ToString());
+                    return (state.Buttons.Y == ButtonState.Pressed) ? 1 : 0;
                 }
+            default: return 0;
+                
         }
-        return 0;
     }
-
 }
