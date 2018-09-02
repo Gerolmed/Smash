aa21776f0c9704788dd54edc94723f35fda54daf
R-Studios <Roman.Wecker@gmx.de>
2018-09-02 12:35:06 +0200

    Update

--- Assets/Scripts/Ingame/Player/NinjaCharacter.cs
+++ Assets/Scripts/Ingame/Player/NinjaCharacter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,25 +14,35 @@ public class NinjaCharacter : Character {
         base.Update();
         if (isNearWall(width / 2, animator.gameObject.transform.rotation.y))
         {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x + pushBack, Mathf.Lerp(rigidbody.velocity.y, slideSpeed, slideAlpha));
             jumpCounter = 0;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, Mathf.Lerp(rigidbody.velocity.y, slideSpeed, slideAlpha));
         }
-	}
+    }
 
     public override void save()
     {
         jump();
     }
 
+    public override void jump()
+    {
+        base.jump();
+        /*if (isNearWall(width / 2, animator.gameObject.transform.rotation.y))
+        {
+            rigidbody.velocity = new Vector2(Mathf.Lerp(rigidbody.velocity.y, slideSpeed, slideAlpha), rigidbody.velocity.y);
+        }*/
+    }
+   
     public bool isNearWall(float Width, float rotation)
     {
         if (rotation == 180)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, rayLengthExtended, LayerHelper.getLayers(new LayerHelper.Layer[] { LayerHelper.Layer.GROUND, LayerHelper.Layer.GOUND_NO_COLLISION }));
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, -rayLengthExtended, LayerHelper.getLayers(new LayerHelper.Layer[] { LayerHelper.Layer.GROUND, LayerHelper.Layer.GOUND_NO_COLLISION }));
             if (hit.collider != null)
             {
                 return true;
             }
+            pushBack = Math.Abs(pushBack);
         }
         else if (rotation == 0)
         {
@@ -40,7 +51,7 @@ public class NinjaCharacter : Character {
             {
                 return true;
             }
-            pushBack = -pushBack;
+            pushBack = -Math.Abs(pushBack);
         }
         return false;
     }
