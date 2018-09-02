5d4d903fc99a5c80c3ec42a4d174f4fe70a3200a
R-Studios <Roman.Wecker@gmx.de>
2018-09-02 12:10:41 +0200

    New Update HYPE

--- Assets/Scripts/Ingame/Player/NinjaCharacter.cs
+++ Assets/Scripts/Ingame/Player/NinjaCharacter.cs
@@ -6,13 +6,14 @@ public class NinjaCharacter : Character {
 	
     public float slideSpeed = -3;
     public float slideAlpha = 3;
+    public float pushBack = 5;
 
 	// Update is called once per frame
 	protected override void Update () {
         base.Update();
         if (isNearWall(width / 2, animator.gameObject.transform.rotation.y))
         {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x + rigidbody.rotation, Mathf.Lerp(rigidbody.velocity.y, slideSpeed, slideAlpha));
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x + pushBack, Mathf.Lerp(rigidbody.velocity.y, slideSpeed, slideAlpha));
             jumpCounter = 0;
         }
 	}
@@ -24,18 +25,22 @@ public class NinjaCharacter : Character {
 
     public bool isNearWall(float Width, float rotation)
     {
-        RaycastHit2D hit;
         if (rotation == 180)
         {
-            hit = Physics2D.Raycast(transform.position, Vector2.left, rayLengthExtended, LayerHelper.getLayers(new LayerHelper.Layer[] { LayerHelper.Layer.GROUND, LayerHelper.Layer.GOUND_NO_COLLISION }));
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, rayLengthExtended, LayerHelper.getLayers(new LayerHelper.Layer[] { LayerHelper.Layer.GROUND, LayerHelper.Layer.GOUND_NO_COLLISION }));
+            if (hit.collider != null)
+            {
+                return true;
+            }
         }
         else if (rotation == 0)
         {
-            hit = Physics2D.Raycast(transform.position, Vector2.right, rayLengthExtended, LayerHelper.getLayers(new LayerHelper.Layer[] { LayerHelper.Layer.GROUND, LayerHelper.Layer.GOUND_NO_COLLISION }));
-        }
-        if (hit.collider != null)
-        {
-            return true;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, rayLengthExtended, LayerHelper.getLayers(new LayerHelper.Layer[] { LayerHelper.Layer.GROUND, LayerHelper.Layer.GOUND_NO_COLLISION }));
+            if (hit.collider != null)
+            {
+                return true;
+            }
+            pushBack = -pushBack;
         }
         return false;
     }
