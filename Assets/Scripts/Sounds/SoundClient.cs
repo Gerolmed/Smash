e927b1c21492c5afff3a96e5f5d49bc58903e2cb
Gerolmed <werwolf420@yahoo.de>
2018-09-13 10:45:25 +0200

    Added Sound Management

---
+++ Assets/Scripts/Sounds/SoundClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundClient : MonoBehaviour {
+
+    public SoundManager.SoundType type;
+
+    [Range(0f,1f)]
+    public float translationValue = 1;
+
+	// Update is called once per frame
+	void Update () {
+        foreach (AudioSource src in GetComponents<AudioSource>())
+        {
+            src.volume = translationValue * SoundManager.getInstance().getSound(type);
+        }
+	}
+}
---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager {
+
+    private Dictionary<int, float> volSettings;
+
+    private static SoundManager instance;
+
+    public static SoundManager getInstance() {
+        if (instance == null)
+            new SoundManager();
+
+        return instance;
+    }
+
+    public SoundManager() {
+        instance = this;
+
+        volSettings = new Dictionary<int, float>();
+    }
+
+    public void setSound(SoundType soundType, float value) {
+        int soundId = (int) soundType;
+        if (volSettings.ContainsKey(soundId))
+            volSettings.Remove(soundId);
+        volSettings.Add(soundId, value);
+    }
+
+    public float getSound(SoundType soundType)
+    {
+        int soundId = (int)soundType;
+
+        if (volSettings.ContainsKey(soundId))
+            return volSettings[soundId];
+
+        return 1f;
+    }
+
+    public enum SoundType {
+        MUSIC
+    }
+}
